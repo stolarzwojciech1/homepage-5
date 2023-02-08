318d4a0d825dbed25b57c7a85dc0b15ecbc72ead
Wojciech Stolarz <stolarzwojciech1@gmail.com>
2023-02-08 17:53:43 +0100

    h

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
+
+const welcome = () =>{
 console.log("Cześć");
+}
 
-let button = document.querySelector(".section__button");
-let body = document.querySelector(".body");
-let themeName = document.querySelector(".themeName");
+welcome();
+
+const button1 = () => {
+const themeName = document.querySelector(".themeName");
+const body = document.querySelector(".body");
+
+body.classList.toggle("body__dark");
+themeName.innerText = body.classList.contains("body__dark") ? "Tło 1" : "Tło 2";
 
 
-button.addEventListener("click", () => {
-    body.classList.toggle("body__dark");
+}
 
-    themeName.innerText = body.classList.contains("body__dark") ? "Tło 1" : "Tło 2";
-});
+let init = () => {const button = document.querySelector(".section__button");
+
+button.addEventListener("click", button1);
+
+
+
+}
+init();
